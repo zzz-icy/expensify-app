62db2912f6f0d5919d07b12eeebcee31443d7b72
Han Zhang <eveline1129@163.com>
2018-09-19 18:23:22 -0500

    added unit test to action expense.js, dynamic id

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,65 @@
+// import the function to be tested
+// test file will run through babel set up, so wecan use ES6 import
+
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import uuid from 'uuid';
+
+test('should setup remove expense action object', () => {
+    const action = removeExpense({ id: '123abc' });
+    //Use .toEqual to compare recursively all properties of object instances (also known as "deep" equality). It calls Object.is to compare primitive values, which is even better for testing than === strict equality operator.
+    // object, array -- toEqual
+    //boolean, string,number -- toBe
+    // {} === {}   false
+    // [] === []   false
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
+});
+
+test('shoule setup edit expense action object', () => {
+    const action = editExpense('123abc', { amount: 10 });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+            amount: 10
+        }
+    });
+});
+
+test('should setup add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'rent',
+        note: 'July',
+        amount: 1122,
+        createdAt: 1000
+    };
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            // id is dynamic
+            // we can use expect.any(constructor)
+            // we only care the is is string
+            id: expect.any(String),
+        }
+    });
+});
+
+test('should setup add expense action object with defult values', () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+
+        expense: {
+            id: expect.any(String),
+            description: '',
+            note: '',
+            amount: 0,
+            createdAt: 0,
+        }
+    });
+});
+
