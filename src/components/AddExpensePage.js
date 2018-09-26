96b594975913a3f316b3939ce026b1cce0fd3119
Han Zhang <eveline1129@163.com>
2018-09-25 20:59:57 -0500

    use mapDispatchToProps in AddExpensePage

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import { connect } from 'react-redux';
-import { addExpense } from '../actions/expenses'; // will get error TypeError: (0 , _someModule2.default) is not a function(…)_
+import { addExpense } from '../actions/expenses';
+// will get error TypeError: (0 , _someModule2.default) is not a function(…)_
 
-const AddExpensePage = (props) => (
-    <div>
-        <h1> This is my AddExpensePage component!</h1>
-        <ExpenseForm  // this component can be reused for edit expense
-            onSubmit={(expense) => {
-                // console.log(expense);
-                props.dispatch(addExpense(expense));
-                props.history.push('/');
-            }}
-        />
-    </div>
-);
-export default connect()(AddExpensePage); // then we have dispatch as props
+
+class AddExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        props.onSubmit(expense);
+        props.history.push('/');
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> This is my AddExpensePage component!</h1>
+                <ExpenseForm  // this component can be reused for edit expense
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    onSubmit: (expense) => (dispatch(addExpense(expense))),
+});
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage); // then we have dispatch as props
