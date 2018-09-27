1810f1f42422d0a756a691ec3e18d3433d8e5538
Han Zhang <eveline1129@163.com>
2018-09-27 00:15:01 -0500

    modified webpack.config and package to separate prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,43 @@
 
 const path = require('path');
 // console.log(path.join(__dirname, 'public'));
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    // console.log('env', env);
+    // env undefined
+    // after   "build:prod": "webpack -p --env production"
+    // env production
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'), // has to be absolute
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,   // file ending with .js
-            exclude: /node_modules/
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'), // has to be absolute
+            filename: 'bundle.js'
         },
-        {
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,   // file ending with .js
+                exclude: /node_modules/
+            },
+            {
 
-            test: /\.s?css$/,  //s? makes 's' optional
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true,  // for all unknown 404 not found, to always serve up index.html file
-    }
-};
+                test: /\.s?css$/,  //s? makes 's' optional
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,  // for all unknown 404 not found, to always serve up index.html file
+        }
+    };
+}
 
 
 // loader allows you to customize the behavior of webpack when you load some given files
