2631e3daab66fc95f24b201f7faeea14086bad80
Han Zhang <eveline1129@163.com>
2018-10-18 01:00:12 -0500

    styled login button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,12 +5,13 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = (props) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1 className="box-layout__title">Expensify App</h1>
+            <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your exoenses under control.</p>
             <button
                 onClick={props.startLogin}
+                className="button"
             >
-                Login
+                Login with Google
         </button>
         </div>
     </div>
