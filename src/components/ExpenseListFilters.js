c2f35fc5f92b0d5852bdb8f0b9c300eb601d3cbf
Han Zhang <eveline1129@163.com>
2018-10-18 15:51:49 -0500

    styled filters and make it responsive

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -28,38 +28,45 @@ export class ExpenseListFilters extends React.Component {
 
     render() {
         return (
-            <div>
-                {/*controlled input*/}
-                <input
-                    type='text'
-                    value={this.props.filters.text}
-                    // change state in store
-                    onChange={this.onTextChange}
-                >
-                </input>
-                {/*Warning: Failed form propType: You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.*/}
+            <div className="content-container">
+                {/* make the three filters into three <div>, for mobile friendly*/}
+                <div className="input-group">
+                    <div className="input-group__item">
+                        {/*controlled input*/}
+                        <input
+                            type='text'
+                            value={this.props.filters.text}
+                            // change state in store
+                            onChange={this.onTextChange}
+                        >
+                        </input>
+                        {/*Warning: Failed form propType: You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.*/}
+                    </div>
+                    <div className="input-group__item">
+                        <select
+                            value={this.props.filters.sortBy}
+                            onChange={this.onSortChange}
+                        >
+                            <option value='date'>Date</option>
+                            <option value='amount'>Amount</option>
 
-                <select
-                    value={this.props.filters.sortBy}
-                    onChange={this.onSortChange}
-                >
-                    <option value='date'>Date</option>
-                    <option value='amount'>Amount</option>
-
-                </select>
-
-                <DateRangePicker
-                    startDate={this.props.filters.startDate}
-                    startDateId="start" // needed for the latest version of react-dates
-                    endDate={this.props.filters.endDate}
-                    endDateId="end"
-                    onDatesChange={this.onDatesChange}
-                    focusedInput={this.state.calendarFocused}
-                    onFocusChange={this.onFocusChange}
-                    showClearDates={true}
-                    numberOfMonths={1}
-                    isOutsideRange={() => { false }} // so that we can choose past date
-                />
+                        </select>
+                    </div>
+                    <div className="input-group__item">
+                        <DateRangePicker
+                            startDate={this.props.filters.startDate}
+                            startDateId="start" // needed for the latest version of react-dates
+                            endDate={this.props.filters.endDate}
+                            endDateId="end"
+                            onDatesChange={this.onDatesChange}
+                            focusedInput={this.state.calendarFocused}
+                            onFocusChange={this.onFocusChange}
+                            showClearDates={true}
+                            numberOfMonths={1}
+                            isOutsideRange={() => { false }} // so that we can choose past date
+                        />
+                    </div>
+                </div>
             </div>
         )
     };
